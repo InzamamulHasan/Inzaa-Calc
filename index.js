8a6a0184efd50e7ae3052d0fcd13dad010962bd3
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-03-31 11:47:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let a=document.getElementsByClassName("items");
+let ans="";
+let ans2=""
+for(let i=0;i<a.length;i++){
+    a[i].addEventListener("click",function(){
+        let str=a[i].innerText;
+        ans2+=str;
+        if(str==="x"){
+            str="*";
+        }else if(str==="%"){
+            str="/";
+        }
+        ans+=str;
+        //console.log(ans);
+        document.querySelector(".c").innerText=ans2;
+    });
+}
+
+let equ=document.querySelector(".items2");
+var sum="";
+equ.addEventListener("click",function(){
+    
+    if(ans!==""){
+        sum=eval(ans);
+    }
+    document.querySelector(".c").innerText=sum;
+    ans="";
+    ans2="";
+})
+
+
+
+
